2dac7adfe73272dd4deb0a23106450c12c09d2ac
Russell Porter <contact@russellporter.com>
2012-03-12 21:56:00 -0700

    A little more progress.. hacking a way at knockout :)

--- result_viewer.js
+++ result_viewer.js
@@ -1,21 +1,21 @@
 // Sample: http://whyjustrun.ca/iof/3.0/events/746/result_list.xml
 var wjr = {};
 wjr.IOF = {};
-wjr.IOF.Event = function(id, name, startTime, courses) {
+wjr.IOF.Event = function(id, name, startTime) {
 	this.id = id;
 	this.name = name;
 	this.startTime = startTime;
-	this.courses = [];
 }
 	
 wjr.IOF.Course = function(id, name, results) {
 	this.id = id;
 	this.name = name;
-	this.results = [];
+	this.results = ko.observableArray(results);
 }
 	
 wjr.IOF.Result = function(time, person) {
 	this.time = time;
+	this.person =person;
 }
 
 wjr.IOF.Person = function(id, givenName, familyName) {
@@ -29,33 +29,36 @@ wjr.IOF.loadResultsList = function(xml) {
 	var event = resultList.children("Event").first();
 	var references = resultList.children("References").first();
 	var courses = [];
-	resultList.each('ClassResult', function(index, element) {
+	resultList.children('ClassResult').each(function(index, element) {
+		element = $(element);
 		var courseId = element.children("Class").attr("idref");
 		var courseName = courseId; // NOTE-RWP Temporary
 		var results = [];
-		element.each('PersonResult', function(index, element) {
-			var person = element.children("Person").item(0);
-			var personGivenName = person.children("Name > Given").text();
-			var personFamilyName = person.children("Name > Family").text();
+		element.children('PersonResult').each(function(index, element) {
+			element = $(element);
+			var person = element.children("Person").first();
+			var personGivenName = person.children("Name").children("Given").text();
+			var personFamilyName = person.children("Name").children("Family").text();
 			var personId = person.children("Id").text();
 			// TODO-RWP Process resultTime
-			var resultTime = element.children("Result > Time").text();
-			var resultPosition = element.children("Result > Position").text();
+			var resultTime = element.children("Result").children("Time").text();
+			var resultPosition = element.children("Result").children("Position").text();
 			results.push(new wjr.IOF.Result(resultTime, new wjr.IOF.Person(personId, personGivenName, personFamilyName)));
 		});
 		courses.push(new wjr.IOF.Course(courseId, courseName, results));
 	})
 	// TODO-RWP startTime
-	return new wjr.IOF.Event(event.children("Id").text(), event.children("Name").text(), nil, courses);
+	return [new wjr.IOF.Event(event.children("Id").text(), event.children("Name").text(), null), courses];
 }
 
 $(function() {
-	var createResultsList = function(id, url) {
+	var createResultList = function(element, url) {
 		var viewModel = {
-			event : ko.observable()
+			event : ko.observable(),
+			courses : ko.observableArray()
 		};
 		
-		ko.applyBindings(viewModel);
+		ko.applyBindings(viewModel, element);
 	
 		$.ajax({
 			type: "GET",
@@ -75,14 +78,16 @@ $(function() {
 			},
 			success: function(xml)
 			{
-				viewModel.event = wjr.IOF.loadResultsList(xml);
+				result = wjr.IOF.loadResultsList(xml);
+				viewModel.event(result[0]);
+				viewModel.courses(result[1])
 			}
 		});
 	}
 	
-	function loadResultsList() {
-		loadResultFromUrl(this.attr("id"), this.attr("data-results-list-url"));
+	function loadResultList(index, element) {
+		createResultList(element, this.getAttribute("data-result-list-url"));
 	}
 	
-	$("results-list").each(loadResultsList());
+	$(".result-list").each(loadResultList);
 });
